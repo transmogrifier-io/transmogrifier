8dc781868c849863e7b8966cbc889b44c8319e7b
felixng <felixngb@outlook.com>
2023-05-11 15:30:23 -0700

    Added oden to transmog filter with Brian and Ramil

--- transmogrifier.js
+++ transmogrifier.js
@@ -413,6 +413,9 @@ const filters =
     },
     to_lower: async function (data, params) {
         return data.toLowerCase()
+    },
+    oden_to_transmogrifier: async function (data, params) {
+        return odenToTransmog(data, params);
     }
 };
 
@@ -522,6 +525,31 @@ async function transmogrify(manifest) {
             await transmogrifyEntry(entry, schemaEntry.schema);
         }
     }
+}
+
+function odenToTransmog(data, params) {
+    if (typeof data === 'string' || data instanceof String) {
+        data = JSON.parse(data);
+    }
+
+    let output_obj = {schema: "", entries: []};
+    let category = params.category
+    data.map((item) => {
+        if (item.labels.category === category) {
+            output_obj.schema = item.data.schema;
+
+            let entry_obj = {};
+            if (item.data.datasets.json.url){
+                entry_obj.source = {func: "url_read", params: {path: item.data.datasets.json.url}};
+            }
+            if (item.data.datasets.json.filters.json){
+                entry_obj.filters = {func: item.data.datasets.json.filters.json, params:{library: "https://raw.githubusercontent.com/OpendataDeveloperNetwork/ODEN-Transmogrifiers/dev/libraries/standard.js"}};
+            }
+            output_obj.entries.push(entry_obj);
+        }
+    })
+    return JSON.stringify(output_obj, null, 2);
+    
 }
 
 module.exports =

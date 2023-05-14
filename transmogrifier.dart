6726df8d2ee41246e8ec1639a80ae060ea796a30
Markus Sample <markussample@gmail.com>
2023-05-13 17:38:05 -0700

    get transmog working

--- transmogrifier.dart
+++ transmogrifier.dart
@@ -3,7 +3,6 @@ import 'dart:convert';
 import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_js/flutter_js.dart';
-import 'package:flutter_js/javascript_runtime.dart';
 // http and https
 // ajv
 
@@ -17,13 +16,9 @@ Future<dynamic> readFile(String filePath) {
   return src.readAsString();
 }
 
-Future<dynamic> readURL(String url) async {
-  // should read a url
-  print('Reading URL: $url');
-  String authority = "raw.githubusercontent.com";
-  String unEncodedPath = url.substring(34);
-  print('unEncodedPath: $unEncodedPath');
-  var response = await http.get(Uri.http(authority, unEncodedPath));
+Future<dynamic> readURL(String uri_string) async {
+  Uri uri = Uri.parse(uri_string);
+  var response = await http.get(Uri.http(uri.authority, uri.path));
   return response.body;
 }
 
@@ -46,9 +41,8 @@ Future<void> writeURL(String url, dynamic data) async {
   // should write data to a url
   var postURL = Uri.https(url, '');
   var response = await http.post(postURL, body: data);
-  // print('Response status: ${response.statusCode}');
-  // print('Response body: ${response.body}');
-  JsEvalResult result = await jsRuntime.evaluate('response.body');
+
+  JsEvalResult result = jsRuntime.evaluate('response.body');
   return result.rawResult;
 }
 
@@ -85,119 +79,93 @@ Map sinks = {
   }
 };
 
-/// Runs a function in the JS runtime environment
-dynamic runJS(String function, String functionName, dynamic params) async {
-  String functionCall = '${functionName}(${params[0]},${params[1]})';
-  print("functionCall = $functionCall");
-  JsEvalResult result = await jsRuntime.evaluate('$function + ${functionName}(${params[0]},${params[1]})');
-  return result.stringResult;
-} 
+dynamic runJSFilter(dynamic data) async {
+  JsEvalResult filterResult = jsRuntime.evaluate("""JSON.stringify(filter(${jsonEncode(data)}, params), null);""");
+  
+  return jsonDecode(filterResult.toString());
+}
 
 // data comes in as a string, leaves as a Map (I think types could be messed up here)
-Future<Map> runPipelineEntry(Function sourceFunc, Map sourceParams, List filters, Function sinkFunc, Map sinkParams, String schema) async {
+Future<dynamic> runPipelineEntry(Function sourceFunc, Map sourceParams, List filters, Function sinkFunc, Map sinkParams, String schema) async {
   dynamic data = await sourceFunc(sourceParams);
 
   for (Map filter in filters) {
     dynamic filterFunc = await getFilterFunction(filter['func']);
-    dynamic filterParams = [];
-    if (filter['params'] == null) {
-      Map filterParams = await getFilterParameters(filter['params']);
-      filterParams['schema'] = schema;
+    Map filterParams = await getFilterParameters(filter['params'] ?? {});
+    filterParams['schema'] = schema;
+    if (filterFunc is Function) {
+      data = await filterFunc(data, filterParams);
+    } else {
+      data = await runJSFilter(data);
     }
-    // Map filterParams = await getFilterParameters(filter['params'] ?? {});
-    // filterParams['schema'] = schema;
-    data = await runJS(filterFunc, "filter", filterParams);
-    print('data = $data');
-    // data = await filterFunc(data, filterParams);
   }
   await sinkFunc(sinkParams, data);
   return data;
 }
 
-Future<List> runPipelineSchemaEntry(List data, List filters, Function sinkFunc, Map sinkParams, String schema) async {
+Future<dynamic> runPipelineSchemaEntry(dynamic data, List filters, Function sinkFunc, Map sinkParams, String schema) async {
   for (Map filter in filters) {
     dynamic filterFunc = await getFilterFunction(filter['func']);
-    Map filterParams = await getFilterParameters(filter['params'] ? filter['params'] : {});
+    Map filterParams = await getFilterParameters(filter['params'] ?? {});
     filterParams['schema'] = schema;
-    data = await filterFunc(data, filterParams);
+    if (filterFunc is Function) {
+      data = await filterFunc(data, filterParams);
+    } else {
+      data = await runJSFilter(data);
+    }
   }
+
   await sinkFunc(sinkParams, data);
   return data;
 }
 
 Future<Function> getSourceFunction(String name) async {
-  Function source; 
-
-  if (name.startsWith('http://') || name.startsWith('https://')) {
-    source = await readURL(name);
-    // parse js
-  } else {
-    source = sources[name];
-  }
+  Function source = sources[name];
 
   return source;
 }
 
 Future<dynamic> getFilterFunction(String name) async {
-  count++;
-  print("Called getFilterFunction for the $count time");
-  dynamic filter; 
-
   if (name.startsWith('http://') || name.startsWith('https://')) {
-    String response = await readURL(name);
-    String trimmedResponse = response.substring(0, response.length - 14);
-    String evaluationString = trimmedResponse + '\nfilter();';
-    filter = trimmedResponse;
+    String filterStr = await readURL(name);
 
-    List<String> lines = trimmedResponse.split('\n');
-    for (String line in lines) {
-      print(line);
-    }
+    jsRuntime.evaluate("""var get = function() {$filterStr}; var filter = get();""");
+    return filterStr;
 
-    JsEvalResult result = await jsRuntime.evaluate(trimmedResponse + 'filter();');
-    print('result = ${result.stringResult}');
-    // parse js
   } else {
-    filter = filters[name];
+    return filters[name];
   }
-  
-  return filter;
 }
 
 Future<Function> getSinkFunction(String name) async {
-  Function sink; 
-
-  if (name.startsWith('http://') || name.startsWith('https://')) {
-    sink = await readURL(name);
-    // parse js
-  } else {
-    sink = sinks[name];
-  }
+  Function sink = sinks[name];  
 
   return sink;
 }
 
 Future<String> getSchema(String path) async {
   String schema = await readURLOrFile(path);
+  jsRuntime.evaluate("""var schema = $schema;""");
   return schema;
 }
 
 Future<Map> getFilterParameters(Map params) async {
+  jsRuntime.evaluate("""var params = {};""");
+  
   if (params['validator'] == 'json') {
     // some validator
   }
 
-  if (params['library']) {
+  if (params.containsKey('library')) {
     String library = await readURLOrFile(params['library']);
-    print('library = $library');
-    // parse js
+    params['library'] = library;
+    jsRuntime.evaluate("""var get = function() {$library}; params.library = get();""");
   }
 
   return params;
 }
 
-Future<Map> transmogrifyEntry(Map entry, String schemaPath) async {
-  print("Transmogrifying entry: $entry");
+Future<dynamic> transmogrifyEntry(Map entry, String schemaPath) async {
   Map source = entry['source'];
   List filters = entry['filters'];
   Map sink = entry['sink'] ?? {'func': 'null', 'params': {}};
@@ -206,11 +174,10 @@ Future<Map> transmogrifyEntry(Map entry, String schemaPath) async {
   Function sourceFunc = await getSourceFunction(source['func']);
   Function sinkFunc = await getSinkFunction(sink['func']);
 
-  print("Running pipeline for entry");
   return runPipelineEntry(sourceFunc, source["params"], filters, sinkFunc, sink['params'], schema);
 }
 
-Future<List> transmogrifySchemaEntry(List data, Map schemaEntry) async {
+Future<dynamic> transmogrifySchemaEntry(List data, Map schemaEntry) async {
   List filters = schemaEntry['filters'] ?? [];
   Map sink = schemaEntry['sink'] ?? {'func': 'null', 'params': {}};
 
@@ -225,34 +192,23 @@ Future<List> transmogrify(List manifest) async {
   for (Map schemaEntry in manifest) {
     List entryDatas = [];
     for (Map entry in schemaEntry['entries']) {
-      Map entryData = await transmogrifyEntry(entry, schemaEntry['schema']);
+      dynamic entryData = await transmogrifyEntry(entry, schemaEntry['schema']);
       entryDatas.add(entryData);
     }
-    List schemaEntryData = await transmogrifySchemaEntry(entryDatas, schemaEntry);
+    dynamic schemaEntryData = await transmogrifySchemaEntry(entryDatas, schemaEntry);
     schemaEntryDatas.add(schemaEntryData);
   }
 
   return schemaEntryDatas;
 }
 
-void main() async {
+Future<List> main() async {
   // File manifest_file = File("assets\\van-texas-manifest.json");
   String manifest_string = await rootBundle.loadString("assets/van-texas-manifest.json");
   // print('Manifest: $manifest_string');
   // String manifest_string = await manifest_file.readAsString();
   List manifest = jsonDecode(manifest_string);
 
-  transmogrify(manifest);
+  List data = await transmogrify(manifest);
+  return data;
 }
-
-// void main() async {
-//   // Get the system temp directory.
-//   var systemTempDir = Directory.systemTemp;
-
-//   // List directory contents, recursing into sub-directories,
-//   // but not following symbolic links.
-//   await for (var entity in
-//       systemTempDir.list(recursive: true, followLinks: false)) {
-//     print(entity.path);
-//   }
-// }

aecd77ccdb858acf1209d0b7545cb5aa2174dce8
Sarah Semkow <ssemkow@my.bcit.ca>
2023-11-15 23:07:09 -0800

    Created ExampleMain to run java transmogrifier

---
+++ ExampleMain.java
@@ -0,0 +1,117 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import transmogrifier2.Transmogrifier2;
+
+public class ExampleMain {
+
+  interface ManifestCallback {
+      void onComplete(String data, Exception error);
+  }
+
+  static CompletableFuture<String> transmogrifyAsync(String input) {
+    CompletableFuture<String> future = new CompletableFuture<>();
+    // Your asynchronous transmogrification logic here
+    // Replace the sleep with your actual asynchronous logic.
+    new Thread(() -> {
+      try {
+        Thread.sleep(2000); // Simulating asynchronous processing
+        future.complete(input.toUpperCase());
+      } catch (InterruptedException e) {
+        future.completeExceptionally(e);
+      }
+    }).start();
+    return future;
+  }
+
+  static void loadManifest(String manifestPath, ManifestCallback callback) {
+    if (manifestPath.startsWith("http://") || manifestPath.startsWith("https://")) {
+      try {
+        URL url = new URL(manifestPath);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+
+        if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+          try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+            StringBuilder rawData = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+              rawData.append(line);
+            }
+
+            callback.onComplete(rawData.toString(), null);
+          }
+        } else {
+          callback.onComplete(null, new Exception("Failed to load manifest: HTTP status code " + connection.getResponseCode()));
+        }
+      } catch (IOException e) {
+          callback.onComplete(null, e);
+      }
+    } else {
+      try {
+        byte[] data = Files.readAllBytes(Paths.get(manifestPath));
+        callback.onComplete(new String(data, "UTF-8"), null);
+      } catch (IOException e) {
+        callback.onComplete(null, e);
+      }
+    }
+  }
+
+  private static List<JSONObject> parseManifest(String manifest) {
+    List<JSONObject> manifestList = new ArrayList<>();
+    JSONParser parser = new JSONParser();
+    try {
+      Object obj = parser.parse(new StringReader(manifest));
+      if (obj instanceof JSONArray) {
+        JSONArray jsonArray = (JSONArray) obj;
+        for (Object item : jsonArray) {
+          if (item instanceof JSONObject) {
+            manifestList.add((JSONObject) item);
+          }
+        }
+      }
+    } catch (Exception e) {
+      System.err.println(e);
+    }
+
+    return manifestList;
+  }
+
+  public static void main(String[] args) {
+    if (args.length == 0) {
+      System.out.println("Please provide an argument");
+      System.exit(1);
+    }
+
+    String manifestUrl = args[0];
+
+    loadManifest(manifestUrl, (data, error) -> {
+      if (error != null) {
+        System.err.println(error);
+        System.exit(1);
+      } else {
+        List<JSONObject> manifestList = parseManifest(data);
+        Transmogrifier2 transmogrifier = new Transmogrifier2(manifestList);
+        try {
+          // String transmogrifiedData = transmogrifier.transmogrify(data);
+          // System.out.println("Transmogrified Data: " + transmogrifiedData);
+        } catch (Exception e) {
+          System.out.println(e);
+        }
+      }
+    });
+  }
+}
